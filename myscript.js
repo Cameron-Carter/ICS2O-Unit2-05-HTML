f703d5faee843d16dc7b52876e525b7e5fb1aca8
Ubuntu <ubuntu@ip-172-31-84-110.ec2.internal>
2020-03-22 19:34:37 +0000

    done

--- myscript.js
+++ myscript.js
@@ -1,9 +1,18 @@
 // JavaScript File
 // let statements for variables
-let squareLength = document.getElementById("numberinput").value;
-let squareArea = squareLength*squareLength;
+let squareLength;
+let squareArea;
+// function to load background color
+function backgroundColour() {
+    window.getElementById('body').className="body";
+}
 // function to square the inputted length, and then alert the area
 function findArea() {
+    squareLength = document.getElementById('numberinput').value;
+    squareArea = squareLength*squareLength;
     alert("The area of the square is " + squareArea);
 }
-document.getElementById("mybutton").addEventListener("click", findArea);
+// event listener to change background color when the page loads
+document.addEventListener('load', backgroundColour);
+// event listener to start findArea function when a double click occurs
+document.addEventListener('dblclick', findArea);
